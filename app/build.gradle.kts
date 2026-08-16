a54c25312b7ca0c240dd9212cb34c308d0ab6a92
KangRio <masolae.sa@gmail.com>
2026-08-17 01:38:36 +0700

    chore: bump version 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.kangrio.byd.assistant"
         minSdk = 24
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
         ndk {
             abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86_64")
         }
